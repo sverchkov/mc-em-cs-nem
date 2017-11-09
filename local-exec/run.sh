5c05a9eac1901bb33e2147762d4b52ba114b268b
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2017-11-09 12:11:19 -0600

    some edits to local-exec

--- local-exec/run.sh
+++ local-exec/run.sh
@@ -3,21 +3,22 @@
 gen_k_lim=5
 learn_k_lim=8 #10
 n_list="3 5 10 25"
-reps=5 #50
+#reps=5 #50
+reps="14 15 32 39 50"
 
 mkdir -p rdata
 
-for r in `seq 1 ${reps}`; do
+for r in ${reps}; do
 	for k in `seq 1 ${gen_k_lim}`; do
 		for n in ${n_list}; do
 			id="$n-$k-$r"
-			dfile="data-$id.RData"
+			dfile="data-${id}.RData"
 			for l in `seq 1 ${learn_k_lim}`; do
-				id2="$id-$l"
-				mfile="model-$id2.RData"
-				if ! [[ -s rdata/$mfile ]]; then
-					echo running $id2
-					rscript R/run.learner.R rdata/$dfile $l rdata/$mfile
+				id2="${id}-$l"
+				mfile="model-${id2}.RData"
+				if ! [[ -s rdata/${mfile} ]]; then
+					echo running ${id2}
+					rscript R/run.learner.R rdata/${dfile} $l rdata/${mfile}
 				fi
 			done;
 		done;
